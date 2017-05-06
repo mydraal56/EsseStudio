2731dab25b2d51973155dcb5401ae2aada0b6151
mydraal56 <mydraal56@hotmail.com>
2017-05-06 14:24:41 -0400

    Rebuilt alot
    It now no longer has any dependency on WP

--- EsseStudio-Janelle/scripts.js
+++
@@ -1,3 +0,0 @@
-//alert('this is a test');
-//this is the test
-//test test
---
+++ MainTestingFolder/scripts.js
@@ -0,0 +1,9 @@
+//alert('this is a test');
+//this is the test
+//test test
+
+function pageLoad(page) { 
+    var g = document.getElementById("MainPage");
+    
+    $("#MainPage").load(page);
+}
