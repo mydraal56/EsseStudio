bd642f84d017da8e9f5d965d8c0b61c7810c84f1
mydraal56 <mydraal56@hotmail.com>
2017-11-24 18:28:03 -0500

    Fixed the slide out menu
    CSS and transitions have it working and looking good in the emulator.
    Posting and testing the look on the phones and live site.

--- scripts.js
+++ scripts.js
@@ -16,19 +16,37 @@ app.config(function($routeProvider){
 function menuBar() { 
     //alert('running menuBar function');
    // debugger;
-    var menu = document.getElementById("menuList");
-    var primary = document.getElementById("primary");
+    var menu = document.getElementById("secondary");
+    //var menuList = document.getElementsByClassName("menu");
+    var menuItems = document.getElementsByClassName("menu-li");
+    //var primary = document.getElementById("primary");
 
     console.log(menu.style.display)
-    if(menu.style.display != 'block')
+    if(menu.style.width != '40%')
     {
-        menu.style.display = "block";
-        primary.style.display = "none";
+        //debugger; 
+        menu.style.width = "40%";
+        setTimeout(function(){
+            for (var i = 0; i < menuItems.length; i++)
+            {
+                //menuItems[i].attributes.transition-delay(2s);
+                menuItems[i].style.width = "100%";
+               // menuItems[i].style.display = 'block';
+                
+            }
+        }, 500); 
+      //  primary.style.display = "none";
     }
     else
     { 
-        menu.style.display = 'none';
-        primary.style.display = 'block';
+        
+        for (var i = 0; i < menuItems.length; i++)
+        {
+            menuItems[i].style.width = "0";
+            //menuItems[i].style.display = "none";
+        }
+        menu.style.width = "0"; 
+      //  primary.style.display = 'block';
     }
     
     //document.getElementById("menu-container").style.display = "block";
